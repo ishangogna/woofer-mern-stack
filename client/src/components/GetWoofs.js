1ebbf51a48eb6ae7505b3fbac72a67489b6b396e
Ishan Gogna <ishangogna10@gmail.com>
2020-07-25 14:02:31 +0530

    fetch/get integrated

--- client/src/components/GetWoofs.js
+++ client/src/components/GetWoofs.js
@@ -1,10 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const GetWoofs = () => {
-
+    const [woofs, setWoofs] = useState([]);
+    
+    useEffect(()=> {
+        const url = "http://localhost:2000/api/woofs";
+        fetch(url)
+            .then(response => response.json())
+            .then(data => { setWoofs(data) })
+            .catch(err => console.log(err));
+    }, [])
     return ( 
         <div>
-            Woofs will go here.
+            {woofs.map(woof=>(
+                <div className = "woof" key = {woof._id}>
+                    <div className = "woof-name">{woof.name}</div>
+                    <div className = "woof-content">{woof.content}</div>
+                    <div className = "woof-createdAt">{woof.createdAt}</div>
+                </div>
+            ))} 
         </div>
      );
 }
--- client/src/components/WoofEntry.js
+++ client/src/components/WoofEntry.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
 
 const WoofEntry = () => {
+    const [name, setName] = useState('');
+    const [content, setContent] = useState('');
+    const addWoof = (e) => {
+        e.preventDefault();
+        const body = name && content ? {name,content} : null
+        const url = "http://localhost:2000/api/woofs";
+        fetch(url, {
+            method : "POST",
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(body)
+        }).then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log('error : ' + err));
+    }
     return ( 
         <form>
             <label htmlFor="name">Name</label>
-            <input type="text" name = "name"/>
-            <textarea type="text" name = "content"/>
+            <input type="text" name = "name" onChange = {(e)=>setName(e.target.value)} required/>
+            <label htmlFor="content">Woof</label>
+            <textarea type="text" name = "content" onChange = {(e) => setContent(e.target.value)}/>
+            <input type="submit" value = 'Add a woof!🐕' onClick = {addWoof}/>
         </form>
      );
 }
